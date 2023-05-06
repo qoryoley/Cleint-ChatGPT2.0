62a6243805017ad86fee3f3378ef2fe69440343e
Abdalla <qoryoley@gmail.com>
2023-05-06 23:03:05 +0300

    initial

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const fetchResponse =  async(chat) => {
     try {
         // after depoloyment you should change the fetch URL below
-        const response = await fetch('https://server-chatgpt-youtube-liart.vercel.app/', { 
+        const response = await fetch('http://localhost:3080', { 
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
